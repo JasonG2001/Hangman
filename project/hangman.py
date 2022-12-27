198ba302089c3f58fadbaba6349363856173a312
jason <xiaohemc2001@gmail.com>
2022-12-27 12:49:40 +0000

    Put down time complexities of the different functions

--- project/hangman.py
+++ project/hangman.py
@@ -1,3 +1,5 @@
+from typing import Set
+
 class Hangman:
 
     def __init__(self, total_lives: int) -> None:
@@ -7,32 +9,35 @@ class Hangman:
 
     def ask_for_word(self) -> None: # O(1)
 
-        word: str = input("Enter your word: ") # O(1)
+        word: str = input("Enter your word: ").lower() # O(n)
         hidden_word: str =  len(word) * "_" # O(1)
         print(hidden_word)
         return self.play(hidden_word, word)
 
 
-    def play(self, current_word: str, word: str, chances: int=0):
+    def play(self, current_word: str, word: str, chances: int=0) -> int: #O (n^3)
 
-        if current_word == word:
-            return "You win"
+        if current_word == word: # O(n)
+            return "You win!"
+        if chances == self.lives: # O(1)
+            return "You lose!"
 
-        if chances == self.lives:
-            return "You lose"
+        letter_guessed: str = input("Guess a letter: ").lower() # O(n)
 
-        letter_guessed: str = input("Guess a letter: ")
-
-        if letter_guessed in word:
+        letters_in_word: Set(str) = set(word) # O(n)
+        letters_in_current_word: Set(str) = set(current_word) # O(n)
+        if letter_guessed in letters_in_word and letter_guessed not in letters_in_current_word: # O(1)
             i: int
             letter: str
-            for i, letter in enumerate(word):
-                if letter == letter_guessed:
-                    current_word: str = current_word[:i] + letter + current_word[i+1:]
+            for i, letter in enumerate(word): # O(n)
+                if letter == letter_guessed: # O(1)
+                    current_word: str = current_word[:i] + letter + current_word[i+1:] # O(k+l)
         else:
-            chances += 1
+            if letter_guessed in current_word: # O(n)
+                print("You've already guessed this letter!!")
+            chances += 1 # O(1)
+            print(f"You have {self.lives - chances} lives left")
 
         print(current_word)
-        print(f"You have {self.lives - chances} lives left")
 
         return self.play(current_word, word, chances)
