0bf961cb8fd6d9816132cc584fbf33e48c06c583
jason <xiaohemc2001@gmail.com>
2022-12-27 11:38:13 +0000

    Make game without score yet

--- project/hangman.py
+++ project/hangman.py
@@ -5,15 +5,27 @@ class Hangman:
         self.lives = 6
 
 
-    def ask_for_word(self):
+    def ask_for_word(self) -> None: # O(1)
 
-        word: str = input("Enter your word: ")
-        hidden_word: str =  len(word) * "_"
-        return hidden_word
-
-
-    def play(self):
-
-        pass
+        word: str = input("Enter your word: ") # O(1)
+        hidden_word: str =  len(word) * "_" # O(1)
+        print(hidden_word)
+        return self.play(hidden_word, word)
 
 
+    def play(self, current_word: str, word: str):
+
+        if current_word == word:
+            return "You win"
+
+        letter_guessed: str = input("Guess a letter: ")
+
+        i: int
+        letter: str
+        for i, letter in enumerate(word):
+            if letter == letter_guessed:
+                current_word: str = current_word[:i] + letter + current_word[i+1:]
+
+        print(current_word)
+
+        return self.play(current_word, word)
